0636fa4e48d525e669daa0466849ea77c390b9e6
Bar Horing Amir <bar.apple@gmail.com>
2020-08-12 14:54:10 +0300

    adding increment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import './styles.scss';
+
+const INITIAL_STATE = {
+  value: 0,
+};
+
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({ type: INCREMENT });
+
+const reducer = (state = {}, action) => {
+  console.log(action, state);
+  if (action.type === INCREMENT) return { value: state.value + 1 };
+  return state;
+};
+
+const store = createStore(reducer, INITIAL_STATE);
 
 class Counter extends Component {
   render() {
